5df35d2ab679c012371a6305a861c50a0f24443c
れみろう <remyroez+github@gmail.com>
2019-05-07 22:00:30 +0900

    :sparkles: キャラクターの仮実装

--- game/src/scenes/Game.lua
+++ game/src/scenes/Game.lua
@@ -5,6 +5,12 @@ local Base = require(folderOfThisFile .. 'Base')
 -- ゲーム
 local Game = Base:newState 'game'
 
+-- ライブラリ
+local wf = require 'windfield'
+
+-- クラス
+local Character = require 'Character'
+
 -- エイリアス
 local lg = love.graphics
 local lk = love.keyboard
@@ -16,18 +22,41 @@ end
 
 -- ステート開始
 function Game:entered(state, ...)
+    -- ワールド
+    state.world = wf.newWorld(0, 0, true)
+    state.world:setGravity(0, 512)
+
+    -- キャラクター
+    state.player = Character {
+        spriteType = 'playerRed',
+        spriteSheet = self.spriteSheet,
+        x = 100,
+        y = 100,
+        offsetY = 16,
+        collider = state.world:newRectangleCollider(0, 0, 24, 32),
+        h_align = 'center',
+        v_align = 'bottom',
+    }
+
+    state.block = state.world:newRectangleCollider(0, 500, 800, 50)
+    state.block:setType('static')
 end
 
 -- ステート終了
 function Game:exited(state, ...)
+    state.player:destroy()
 end
 
 -- 更新
 function Game:update(state, dt)
+    state.world:update(dt)
+    state.player:update(dt)
 end
 
 -- 描画
 function Game:draw(state)
+    state.player:draw()
+    state.world:draw()
 end
 
 -- キー入力
